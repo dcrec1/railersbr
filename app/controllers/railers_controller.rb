bc50374cb47ea66eae89c4d460799a30de862083
Diego Carrion <dc.rec1@gmail.com>
2009-03-14 18:49:08 -0300

    railers controller paginates the model

--- app/controllers/railers_controller.rb
+++ app/controllers/railers_controller.rb
@@ -1,2 +1,8 @@
 class RailersController < ApplicationController
+  
+  def index
+    page = params[:page] || 1
+    @railers = Railer.paginate :page => page, :order => "name", :size => 15
+  end
+  
 end
--- spec/controllers/railers_controller_spec.rb
+++ spec/controllers/railers_controller_spec.rb
@@ -2,9 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe RailersController do
 
-  #Delete this example and add some real ones
-  it "should use RailersController" do
-    controller.should be_an_instance_of(RailersController)
+  context "responding to GET index" do
+    
+    it "should paginate 15 railers by the name" do
+      page = '4'
+      Railer.should_receive(:paginate).with(:size => 15, :order => 'name', :page => page)
+      get 'index', :page => page
+    end
+    
+    it "should assign railers with the pagination" do
+      value = "432432"
+      Railer.stub!(:paginate).and_return(value)
+      get 'index'
+      assigns[:railers].should eql(value)
+    end
+    
+    it "should assume first page case none is specified" do
+      Railer.should_receive(:paginate).with(hash_including(:page => 1))
+      get 'index'
+    end
+    
   end
 
 end
