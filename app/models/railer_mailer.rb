c09caf01f0f3201d9d8c1be5ddd9e6141fd710aa
dcrec1 <dc.rec1@gmail.com>
2009-03-19 20:41:35 -0300

    configured mailer

--- app/models/railer_mailer.rb
+++ app/models/railer_mailer.rb
@@ -1,4 +1,10 @@
 class RailerMailer < ActionMailer::Base
-  
+
+  def contact(users)  
+    recipients users
+    from "noreply@mouseoverstudio.com"  
+    subject "Contato mediante RailersBR"  
+    sent_on Time.now
+  end   
 
 end
