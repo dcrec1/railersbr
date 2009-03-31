909d07904275d7757837a8781d4ed1c901e6083e
Diego Carrion <dc.rec1@gmail.com>
2009-03-31 12:36:04 -0300

    refactorings

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -5,30 +5,39 @@ class Manager
 
   def self.update_railers
     Railer.delete_all
-    h = Hpricot(Kernel.open("http://workingwithrails.com/browse/people/country/Brazil"))
-    h.search(".entry-list li").each do |li|
+    html_from("http://workingwithrails.com/browse/people/country/Brazil").search(".entry-list li").each do |li|
       a = li.search('a')
       url = a.attr(:href)
-      email = ''
-      Hpricot(Kernel.open(url)).search("script") do |script|
-        inner_text = script.inner_text
-        inner_text.scan(/eval\(unescape\(\'(.+)\'/) do |str|
-          email = CGI.unescape(str.first).scan(/.+href=\"(.+)\".+/).first.first
-        end if inner_text.include?("eval(unescape")
-      end
-      city = (li.search('span').inner_text.split(',')[2] || '').lstrip
-      Railer.create!(:name => a.inner_text,
-                     :email => email,
+      Railer.create! :name => a.inner_text,
+                     :email => extract_email_from(url),
                      :url => url,
-                     :city => fix(city))
+                     :city => extract_city_from(li)
     end
   end
   
   private
   
-  def self.fix(city)
-    #city.gsub(/[Ss]ao [Pp]aulo/, 'São Paulo')
-    city
+  def self.extract_city_from(li)
+    (li.search('span').inner_text.split(',')[2] || '').lstrip
+  end
+  
+  def self.extract_email_from(url)
+    email = ''
+    html_from(url).search("script") do |script|
+      inner_text = script.inner_text
+      inner_text.scan(/eval\(unescape\(\'(.+)\'/) do |str|
+        email = CGI.unescape(str.first).scan(/.+href=\"(.+)\".+/).first.first
+      end if inner_text.include?("eval(unescape")
+    end
+    email
+  end
+  
+  def self.html_from(url)
+    Hpricot(open(url))
+  end
+  
+  def self.open(url)
+    Kernel.open(url)
   end
   
 end
